10dc64e1bb2d700c8a9f1152dab8c0ddeea5c39d
Elyse Burmester <burmesterelyse@gmail.com>
2018-10-15 13:22:20 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,19 +11,19 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  howarts.
+  howarts.unshift("Ginny")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  hogwarts.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  hogwarts.pop
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  hogwarts[index_number]
 end
 
 def retrieve_first_element_from_array(array)
